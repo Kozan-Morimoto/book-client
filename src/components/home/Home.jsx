eebe45f0a32542f87fbe8a770ea0932f4d94e9ca
Kozan Morimoto <kozan.t.morimoto@gmail.com>
2022-07-12 14:37:45 +0900

    Created home top section

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 import './Home.css'
 
 function Home() {
-  return (
-    <div className='home'>
-        Home
-    </div>
-  )
+	return (
+		<div className='home'>
+			<div className='top'>
+				<div className='main-poster'></div>
+				<a className='top-btn' href='#'>
+					Begin your story.
+				</a>
+			</div>
+		</div>
+	)
 }
 
 export default Home
