c7844711f087db4789953a730862a010bb01caad
Kozan Morimoto <kozan.t.morimoto@gmail.com>
2022-07-12 13:19:51 +0900

    Added navbar animation

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+)
+
+// Toggle function
+window.onload = () => {
+	const nav = document.getElementById('nav')
+	const toggle = document.getElementById('toggle-a')
+	const toggleB = document.getElementById('toggle-b')
+
+	toggle.addEventListener('click', () => {
+		nav.classList.toggle('active')
+	})
+	toggleB.addEventListener('click', () => {
+		nav.classList.toggle('active')
+	})
+}
