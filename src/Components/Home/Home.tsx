0662f191b423adae15571c6d441e8b692c57aa8c
Kozan Morimoto <kozan.t.morimoto@gmail.com>
2022-08-07 15:47:27 +0900

    Made navbar simple for testing, removed main poster

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,27 +1,26 @@
 import React, { useContext } from 'react'
-import {myContext} from '../../Context'
+import { myContext } from '../../Context'
 import '../../GlobalStyles.css'
 import { IUser } from '../../types/maintypes'
-import HomeCSS from './Home.module.css'
+import styles from './Home.module.css'
 import Spotlight from './spotlight/Spotlight'
 
 function Home() {
 	const context = useContext(myContext) as IUser
 	return (
-		<div className={HomeCSS.home}>
-			<div className={HomeCSS.top}>
-				<div className={HomeCSS.mainPoster}></div>
-				<a className={HomeCSS.topBtn} href='/register'>
-					{
-						context ? <p>Welcome back {context.username}</p>
-						: <p>Begin your story {'>'}</p>
-					}
-					
+		<div className={styles.home}>
+			<div className={styles.top}>
+					<div className={styles.mainPoster}></div>
+				<a className={styles.topBtn} href='/register'>
+					{context ? (
+						<p>Welcome back {context.username}</p>
+					) : (
+						<p>Begin your story {'>'}</p>
+					)}
 				</a>
 			</div>
 
 			<Spotlight />
-      
 		</div>
 	)
 }
--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -1,36 +1,17 @@
 import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars, faTimes as faX } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom'
 import NavBarCSS from './NavBar.module.css'
 
 function NavBar() {
 	return (
-		<div className={NavBarCSS.navBar}>
-			<div className={NavBarCSS.bar}>
-				<a href="/"><div className={NavBarCSS.logo}>Logo</div></a>
-
-				<button className={NavBarCSS.toggleA} id='toggle-a'>
-					<FontAwesomeIcon icon={faBars} className={NavBarCSS.faBars} />
-				</button>
-			</div>
-			<nav className={NavBarCSS.nav} id='nav'>
-				<ul className={NavBarCSS.navList}>
-					<li className={NavBarCSS.navItem}>
-						<a href='/'>Home</a>
-					</li>
-					<li className={NavBarCSS.navItem}>
-						<a href='/'>Explore</a>
-					</li>
-					<li className={NavBarCSS.navItem}>
-						<a href='/login'>Login</a>
-					</li>
-
-					<button className={NavBarCSS.toggleB} id='toggle-b'>
-						<FontAwesomeIcon icon={faX} className={NavBarCSS.faTimes} />
-					</button>
-				</ul>
-			</nav>
-		</div>
+		<nav className={NavBarCSS.navBar}>
+			<ul>
+				<span><Link to='/'>Logo</Link></span>
+				
+				<li><Link to='#'>Explore</Link></li>
+				<li><Link to='/login'>Login</Link></li>
+			</ul>
+		</nav>
 	)
 }
 
